6927c92c10d8748ed108030e3d56b56b1a0d2520
rakib-sikder <itsmebroiam@gmail.com>
2024-09-23 22:15:16 +0600

    design home part commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let totaldonate= document.getElementById('totaldonate').innerText;
+        
+
+
+document.getElementById("donatebtn").addEventListener('click', function(){
+    let input = document.getElementById('ammountinput').value;
+    console.log(input); 
+    
+    
+    
+
+})
